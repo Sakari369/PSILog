9e31ff7ea28924ec72adf69fcbd953593ce3074b
Sakari Lehtonen <sakari@psitriangle.net>
2018-04-09 14:53:57 +0300

    Base implementation

--- RightwareLogger/logger.cpp
+++ RightwareLogger/logger.cpp
@@ -12,10 +12,24 @@ void Logger::set_log_level(int log_level) {
 	_log_level = log_level;
 }
 
-int Logger::get_log_level() {
+int Logger::get_log_level() const {
 	return _log_level;
 }
 
-void Logger::set_output(std::ostream *output) {
-	_output = output;
+int Logger::get_log_filter() const {
+	return _log_filter;
+}
+
+void Logger::set_log_filter(int log_filter) {
+	_log_filter = log_filter;
+}
+
+// Flush our output stream to our target destination
+void Logger::flush() {
+	// Log to console for now
+	std::cout << _output_stream.str();
+
+	// Clear it
+	_output_stream.str( std::string() );
+	_output_stream.clear();
 }
--- RightwareLogger/main.cpp
+++ RightwareLogger/main.cpp
@@ -5,10 +5,14 @@
 
 int main(int argc, char *argv[]) {
 	//std::unique_ptr<Logger> logger = std::make_unique<Logger>();
+
 	Logger logger;
-	logger.set_log_level(Logger::INFO || Logger::WARN);
-	logger.set_output(&std::cout);
-	//logger << "All systems initialized";
+	logger.set_log_filter(Logger::INFO | Logger::WARN | Logger::ERR);
+	//logger.set_output_stream(&std::cout);
+
+	logger(Logger::INFO) << "All systems initialized" << std::endl;
+	logger(Logger::WARN) << "WARNING: Phasers damaged" << std::endl;
+	logger(Logger::ERR) << "ERROR: Failed to boot phasers" << std::endl;
 
 	return 0;
 }
